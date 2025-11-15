b28339b7cf0c29258fd6db6c44ca94d7f4a4f3d8
Bitto Kazi <bitto.kazi@gmail.com>
2025-11-15 22:59:29 +0100

    feat(publish): Maven central publish plugin added 💚

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,9 +2,14 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.ktor)
     alias(libs.plugins.kotlin.plugin.serialization)
+
+    // for publishing
+    id("signing")
+    id("maven-publish")
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0"
 }
 
-group = "com.bittokazi.ktor.auth"
+group = "com.bittokazi.sonartype"
 version = "0.0.1.RC1"
 
 application {
@@ -41,4 +46,64 @@ dependencies {
 
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+            pom {
+                name.set("Ktor Oauth Authorization Server")
+                description.set("Oauth Authorization Server Library for Ktor Framework")
+                url.set("https://github.com/bittokazi/ktor-oauth-authorization-server")
+
+                licenses {
+                    license {
+                        name.set("Apache License 2.0")
+                        url.set("https://www.apache.org/licenses/LICENSE-2.0")
+                    }
+                }
+
+                developers {
+                    developer {
+                        id.set("bittokazi")
+                        name.set("Bitto Kazi")
+                        email.set("bitto.kazi@gmail.com")
+                    }
+                }
+
+                scm {
+                    connection.set("scm:git:git://github.com/bittokazi/ktor-oauth-authorization-server.git")
+                    developerConnection.set("scm:git:ssh://github.com/bittokazi/ktor-oauth-authorization-server.git")
+                    url.set("github.com/bittokazi/ktor-oauth-authorization-server")
+                }
+            }
+        }
+    }
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
+            username.set("ftdOLc")
+            password.set("pGm6yirQOsYl4k4HJ0vQfShAJy1werD7j")
+        }
+    }
+}
+
+val signingKey: String? = System.getenv("SIGNING_KEY")
+
+signing {
+    useInMemoryPgpKeys(signingKey, "")
+    sign(publishing.publications["mavenJava"])
 }
