870b69a66001bd6ead3966927f2775464d129249
Bitto Kazi <bitto.kazi@gmail.com>
2025-11-17 09:08:38 +0100

    feat(examples): multi tenant idp server example updated ✨

--- examples/ktor-oauth-server-mutit-tenant-with-two-fa-imlementation/src/main/kotlin/Routing.kt
+++ examples/ktor-oauth-server-mutit-tenant-with-two-fa-imlementation/src/main/kotlin/Routing.kt
@@ -1,14 +1,14 @@
 package com.bittokazi.example.ktor
 
 import com.bittokazi.example.ktor.routes.clientRoutes
+import com.bittokazi.example.ktor.routes.loginRoutes
 import com.bittokazi.example.ktor.routes.otpCheckRoute
 import com.bittokazi.example.ktor.routes.userRoutes
 import io.ktor.server.application.*
 
 fun Application.configureRouting() {
 
-
-
+    loginRoutes()
     userRoutes()
     clientRoutes()
     otpCheckRoute()
