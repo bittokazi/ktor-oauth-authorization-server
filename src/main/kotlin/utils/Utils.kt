fa5ef038116131aa538a95f8e2f6950e33fe75f5
bittokazi <bitto.kazi@gmail.com>
2025-12-05 22:20:45 +0100

    feat: PKCE authorization flow added ✨

--- src/main/kotlin/utils/Utils.kt
+++ src/main/kotlin/utils/Utils.kt
@@ -1,10 +1,24 @@
 package com.bittokazi.ktor.auth.utils
 
+import java.security.MessageDigest
+import java.util.Base64
+
 object Utils {
 
     fun generateUserCode(): String {
         val chars = ('A'..'Z')
         fun block() = (1..4).map { chars.random() }.joinToString("")
         return "${block()}-${block()}"
+    }
+
+    fun verifyPkce(codeVerifier: String, codeChallenge: String): Boolean {
+        val hashed = MessageDigest.getInstance("SHA-256")
+            .digest(codeVerifier.toByteArray(Charsets.UTF_8))
+
+        val computedChallenge = Base64.getUrlEncoder()
+            .withoutPadding()
+            .encodeToString(hashed)
+
+        return computedChallenge == codeChallenge
     }
 }
