b0820f43d3f3afa1e795421d9b7d15e068c65e93
bittokazi <bitto.kazi@gmail.com>
2026-02-11 08:32:13 +0100

    refactor: Fixed typo in example project names ✏️

--- examples/ktor-oauth-server-multi-tenant-with-two-fa-implementation/settings.gradle.kts
+++ examples/ktor-oauth-server-multi-tenant-with-two-fa-implementation/settings.gradle.kts
@@ -1,4 +1,4 @@
-rootProject.name = "ktor-oauth-server-mutit-tenant-with-two-fa-imlementation"
+rootProject.name = "ktor-oauth-server-multi-tenant-with-two-fa-implementation"
 
 dependencyResolutionManagement {
     repositories {
