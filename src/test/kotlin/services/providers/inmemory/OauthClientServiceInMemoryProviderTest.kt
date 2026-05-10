ce1de2cdb6e757f8f0b1a686e213bd8bbddccbc1
bittokazi <bitto.kazi@gmail.com>
2026-05-10 16:11:07 +0200

    feat(auth-route): Authorization code processor provider added, refactoring, bug fixes and test coverage added 🎨 ⚡️ 🐛 ✅

--- src/test/kotlin/services/providers/inmemory/OauthClientServiceInMemoryProviderTest.kt
+++ src/test/kotlin/services/providers/inmemory/OauthClientServiceInMemoryProviderTest.kt
@@ -7,6 +7,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Assert.assertFalse
 import org.junit.Assert.assertNotNull
 import org.junit.Assert.assertNull
+import org.junit.Assert.assertThrows
 import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.mockito.Mockito
