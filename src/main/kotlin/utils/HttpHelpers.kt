ecbff800f3861b81b0e698c4e6a33a4a3361f301
bittokazi <bitto.kazi@gmail.com>
2026-06-14 11:33:05 +0200

    feat(issuer-provider): Added issuer provider :sparkles:

--- src/main/kotlin/utils/HttpHelpers.kt
+++ src/main/kotlin/utils/HttpHelpers.kt
@@ -1,15 +1,26 @@
 package com.bittokazi.ktor.auth.utils
 
+import com.bittokazi.ktor.auth.services.issuer.DefaultIssuerProvider
+import com.bittokazi.ktor.auth.services.issuer.IssuerProvider
+import io.ktor.server.application.Application
 import io.ktor.server.application.ApplicationCall
-import io.ktor.server.plugins.origin
+import io.ktor.server.plugins.di.dependencies
 
-fun ApplicationCall.getBaseUrl(): String {
-    val origin = request.origin
-    val portPart =
-        when {
-            (origin.scheme == "http" && origin.serverPort == 80) -> ""
-            (origin.scheme == "https" && origin.serverPort == 443) -> ""
-            else -> ":${origin.serverPort}"
+var issuerProvider: IssuerProvider? = null
+
+fun Application.configureIssuerProvider() {
+    val issuerProviderObject: IssuerProvider? by dependencies
+    issuerProvider =
+        if (issuerProviderObject == null) {
+            DefaultIssuerProvider()
+        } else {
+            issuerProviderObject
         }
-    return "${origin.scheme}://${origin.serverHost}$portPart"
 }
+
+fun ApplicationCall.getBaseUrl(): String =
+    issuerProvider?.getIssuer(this)
+        ?: run {
+            issuerProvider = DefaultIssuerProvider()
+            issuerProvider!!.getIssuer(this)
+        }
