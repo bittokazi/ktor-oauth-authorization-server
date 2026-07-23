82bf65a1e3c9b6d5996891b5ac43d2a4b01cb437
Bitto Kazi <bitto.kazi@gmail.com>
2026-07-23 07:35:39 +0200

    release: v1.3.2 :bookmark: (#81)

--- examples/ktor-oauth-server-inmemory-implementation/build.gradle.kts
+++ examples/ktor-oauth-server-inmemory-implementation/build.gradle.kts
@@ -41,9 +41,9 @@ dependencies {
     implementation(libs.nimbus.jose.jwt)
 
     if(testKtorOauthAuthLibrary) {
-        implementation(files("../../build/libs/ktor-oauth-authorization-server-1.3.1.jar"))
+        implementation(files("../../build/libs/ktor-oauth-authorization-server-1.3.2.jar"))
     } else {
-        implementation("com.bittokazi.sonartype:ktor-oauth-authorization-server:1.3.1")
+        implementation("com.bittokazi.sonartype:ktor-oauth-authorization-server:1.3.2")
     }
 
     testImplementation(libs.ktor.server.test.host)
