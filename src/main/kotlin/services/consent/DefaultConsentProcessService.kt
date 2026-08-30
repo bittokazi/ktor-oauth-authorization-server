f524918f97b44f748bdd524b4520a73ab1fb51c7
bittokazi <bitto.kazi@gmail.com>
2026-08-30 20:37:18 +0200

    feat: added links to consent denied page ✨

--- src/main/kotlin/services/consent/DefaultConsentProcessService.kt
+++ src/main/kotlin/services/consent/DefaultConsentProcessService.kt
@@ -41,7 +41,6 @@ class DefaultConsentProcessService(
                 ?: return Result.Failure(ConsentFailure.InvalidClient)
 
         if (!client.consentRequired) {
-            oauthLoginOptionService.completeLogin(call)
             return Result.Success(null)
         }
 
@@ -66,7 +65,6 @@ class DefaultConsentProcessService(
                 ),
             )
         } else {
-            oauthLoginOptionService.completeLogin(call)
             Result.Success(null)
         }
     }
@@ -101,7 +99,6 @@ class DefaultConsentProcessService(
                     scopes = client.scopes,
                     call,
                 )
-                oauthLoginOptionService.completeLogin(call)
                 return Result.Success(null)
             }
 
